00375466f6d6afeedb2ee9cd31abe88b0b211874
ravulaprasad <ravulasaisimhaprasad@gmail.com>
2025-01-15 12:46:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,35 +6,35 @@ const FILE_PATH = './data.json';
 
 const makeCommit = n => {
     if (n === 0) {
-        // Push all commits after making them
+        
         return simpleGit().push();
     }
 
-    // Generate a random year between 2017 and 2022
-    const year = Math.floor(Math.random() * (2023 - 2017)) + 2017;
+    
+    const year = Math.floor(Math.random() * (2024 - 2022)) + 2022;
 
-    // Generate a random day of the year
+    
     const dayOfYear = Math.floor(Math.random() * 365);
 
-    // Calculate a random date for the generated year and day of year
+    
     const DATE = moment()
         .year(year)
         .startOf('year')
         .add(dayOfYear, 'days')
-        .format(); // Format the date for Git
+        .format(); 
 
     const data = { date: DATE };
 
-    // Write the random date to a JSON file
+    
     jsonfile.writeFile(FILE_PATH, data, () => {
-        // Stage the file, commit with the date, and recurse
+        
         simpleGit()
             .add([FILE_PATH])
             .commit(DATE, { '--date': DATE }, () => {
-                makeCommit(n - 1); // Recursive call
+                makeCommit(n - 1); 
             });
     });
 };
 
-// Start the commit process with 5000 commits
-makeCommit(5000);
+
+makeCommit(1000);
